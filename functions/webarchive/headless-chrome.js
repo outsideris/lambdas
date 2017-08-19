70356cefac7b3297ce29c17390448462841dd8e0
Outsider <outsideris@gmail.com>
2017-08-19 17:33:59 +0900

    once launch chrome then take snapshots

--- functions/webarchive/headless-chrome.js
+++ functions/webarchive/headless-chrome.js
@@ -3,73 +3,83 @@ const CDP = require('chrome-remote-interface');
 
 const { filenamifyUrl } = require('./util');
 
+let launchedChrome = null;
+const launchChrome = () => {
+  if (launchedChrome) {
+    return new Promise(resolve => resolve(launchedChrome));
+  }
+
+  return chromeLauncher.launch({
+    chromeFlags: [
+      '--headless',
+      '--disable-gpu',
+      '--no-sandbox',
+      '--homedir=/tmp',
+      '--data-path=/tmp/data-path',
+      '--disk-cache-dir=/tmp/cache-dir',
+      '--vmodule', // These two options are needed in lambda
+      '--single-process',
+    ],
+    logLevel: 'verbose',
+  }).then((chrome) => {
+    console.log(`Headless Chrome launched with debugging port ${chrome.port}`);
+    launchedChrome = chrome;
+    return chrome;
+  });
+};
+
 module.exports = {
   screenshot: (url, size = '1366,3000', cb) => {
     console.log(`screenshot: url=${url} size=${size}`);
     const [width, height] = size.split(',').map(d => +d);
 
-    return chromeLauncher.launch({
-      chromeFlags: [
-        '--headless',
-        '--disable-gpu',
-        `--window-size=${size}`,
-        '--no-sandbox',
-        '--homedir=/tmp',
-        '--data-path=/tmp/data-path',
-        '--disk-cache-dir=/tmp/cache-dir',
-        '--vmodule', // These two options are needed in lambda
-        '--single-process',
-      ],
-      logLevel: 'verbose',
-    }).then((chrome) => {
-      console.log(`Headless Chrome launched with debugging port ${chrome.port}`);
+    launchChrome()
+      .then((chrome) => {
+        CDP.Version({ host: 'localhost', port: chrome.port }, (error, info) => {
+          if (error) {
+            console.error(error);
+            return cb(error);
+          }
+          console.log('version: ', info);
 
-      CDP.Version({ host: 'localhost', port: chrome.port }, (error, info) => {
-        if (error) {
-          console.error(error);
-          return cb(error);
-        }
+          return CDP({ host: 'localhost', port: chrome.port }, (client) => {
+            const { DOM, Emulation, Network, Page } = client;
 
-        console.log('version: ', info);
+            Page.enable()
+              .then(DOM.enable)
+              .then(Network.enable)
+              .then(() => {
+                // set up viewport resolution, etc.
+                const deviceMetrics = {
+                  width,
+                  height,
+                  deviceScaleFactor: 0,
+                  mobile: false,
+                  fitWindow: false,
+                };
 
-        return CDP({ host: 'localhost', port: chrome.port }, (client) => {
-          const { DOM, Emulation, Network, Page } = client;
-
-          Page.enable()
-            .then(DOM.enable)
-            .then(Network.enable)
-            .then(() => {
-              // set up viewport resolution, etc.
-              const deviceMetrics = {
-                width,
-                height,
-                deviceScaleFactor: 0,
-                mobile: false,
-                fitWindow: false,
-              };
-
-              return Emulation.setDeviceMetricsOverride(deviceMetrics);
-            })
-            .then(() => Emulation.setVisibleSize({ width, height }))
-            .then(() => Page.navigate({ url })) // navigate to target page
-            .then(Page.loadEventFired)
-            .then(() => Page.captureScreenshot({ format: 'png', fromSurface: true }))
-            .then((screenshot) => {
-              const buffer = new Buffer(screenshot.data, 'base64');
-              client.close();
-              return buffer;
-            })
-            .then(buffer => cb(null, buffer, filenamifyUrl(url)))
-            .catch((err) => {
-              console.error(err);
-              client.close();
-              cb(err);
-            });
-        }).on('error', (err) => {
-          console.error(err);
-          cb(err);
+                return Emulation.setDeviceMetricsOverride(deviceMetrics);
+              })
+              .then(() => Emulation.setVisibleSize({ width, height }))
+              .then(() => Page.navigate({ url })) // navigate to target page
+              .then(Page.loadEventFired)
+              .then(() => Page.captureScreenshot({ format: 'png', fromSurface: true }))
+              .then((screenshot) => {
+                const buffer = new Buffer(screenshot.data, 'base64');
+                client.close();
+                return buffer;
+              })
+              .then(buffer => cb(null, buffer, filenamifyUrl(url)))
+              .catch((err) => {
+                console.error(err);
+                client.close();
+                cb(err);
+              });
+          }).on('error', (err) => {
+            console.error(err);
+            cb(err);
+          });
         });
       });
-    });
   },
 };
--- functions/webarchive/headless-chrome.spec.js
+++ functions/webarchive/headless-chrome.spec.js
@@ -11,5 +11,17 @@ describe('headless chrome', () => {
         return done();
       });
     });
+
+    it('should take multiple screenshots once launching chrome', (done) => {
+      screenshot('https://google.com', undefined, (err) => {
+        if (err) { return done(err); }
+
+        return screenshot('https://github.com', undefined, (err2, buffer) => {
+          if (err2) { return done(err2); }
+          expect(buffer).to.be.an.instanceof(Buffer);
+          return done();
+        });
+      });
+    });
   });
 });
